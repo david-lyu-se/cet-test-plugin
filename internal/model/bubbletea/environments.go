f5efa1fe47ddc53316534733f4d9a20bffa4317d
David-Lyu <lyu.david@yahoo.com>
2024-10-08 22:11:55 -0700

    added update functionality

--- internal/model/bubbletea/environments.go
+++ internal/model/bubbletea/environments.go
@@ -1,19 +1,35 @@
 package bubbletea
 
+import (
+	"github.com/charmbracelet/bubbles/filepicker"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
 type EnvironmentInputs struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
+	file         filepicker.Model
+	selectedFile string
+	Quiting      bool
+	err          error
+	/* Name of App */
 }
 
-func (EnvironmentInputs) init() {
-
+func (EnvironmentInputs) init() tea.Cmd {
+	return nil
 }
 
 func (EnvironmentInputs) update() {
+	// ask user for name of app/environment
+	//
+	// double check
+	//
+	// ask for file path of environment
 
 }
 
 func (EnvironmentInputs) view() {
+
+}
+
+func InitEnvironmentTUI() {
 
 }
--- internal/model/bubbletea/parent.go
+++ internal/model/bubbletea/parent.go
@@ -10,39 +10,55 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type SelectMsg struct {
-	ActiveProjectID uint
+type ParentModel struct {
+	Mode         variables.Mode
+	List         list.Model
+	Input        textinput.Model
+	hasTryDelete bool
+	Quitting     bool
 }
 
-type ParentModel struct {
-	Mode     variables.Mode
-	List     list.Model
-	Input    textinput.Model
-	Quitting bool
-}
+type updateListMsg struct{}
 
 func (parentModel ParentModel) Init() tea.Cmd {
 	//future move operations file create and read here
-	return tea.Cmd
+	return nil
 }
 
 func (parentModel ParentModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
+	var cmd tea.Cmd
 	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
-
+	// tea.msgs default or we can create
 	case tea.WindowSizeMsg:
 		variables.WindowSize = msg
 		top, right, bottom, left := variables.DocStyle.GetMargin()
 		parentModel.List.SetSize(msg.Width-left-right, msg.Height-top-bottom)
-
+	// custom msg for updating Item List after creatihng new Environment
+	case updateListMsg:
+	// tea.msg for keypress
 	case tea.KeyMsg:
-		if key.Matches(msg, variables.Keymap.Quit) {
+		if key.Matches(msg, variables.Keymap.Enter) {
+			//go to plugins model init
+		} else if key.Matches(msg, variables.Keymap.Create) {
+			//go to environment model init
+		} else if key.Matches(msg, variables.Keymap.Delete) {
+			//delete environment tell them to do it manually
+			parentModel.hasTryDelete = true
+		} else if key.Matches(msg, variables.Keymap.Quit) {
 			parentModel.Quitting = true
-			cmds = append(cmds, tea.Quit)
+			cmd = tea.Quit
+		} else {
+			parentModel.List, cmd = parentModel.List.Update(msg)
 		}
-
+		//cant fallthrough
+	default:
+		parentModel.List, cmd = parentModel.List.Update(msg)
+	}
+	if cmd != nil {
+		cmds = append(cmds, cmd)
+		cmd = nil
 	}
 	return parentModel, tea.Batch(cmds...)
 }
@@ -51,6 +67,12 @@ func (parentModel ParentModel) View() string {
 	if parentModel.Quitting {
 		return "Bye"
 	}
+
+	if parentModel.hasTryDelete {
+		parentModel.hasTryDelete = false
+		return variables.DocStyle.Render(parentModel.List.View() + "\nCannot delete items, please go to json file and delete manually")
+	}
+
 	if parentModel.Input.Focused() {
 		return variables.DocStyle.Render(parentModel.List.View() + "\n" + parentModel.Input.View())
 	}
